756684a086d775ed932ba814f912c8fe8d680d58
Natalia Sanchez <natalia@frncubo.com>
2021-04-21 14:03:20 -0300

    fix: init

---
+++ src/components/home/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.css';
+
+function Home() {
+  return (
+    <div className="HomePage">
+        <header className="HomeHeader">
+        <h1>home</h1>
+        </header>
+    </div>
+  );
+}
+
+export default Home;
