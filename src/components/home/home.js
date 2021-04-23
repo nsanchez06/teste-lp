9838478b6d5eebc240b6a48c2f578b998b59a731
Natalia Sanchez <natalia@frncubo.com>
2021-04-23 14:32:39 -0300

    fix: containers

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -10,6 +10,7 @@ import brastemp from '../img/logo-brastemp.png';
 import compraCerta from '../img/logo-compra-certa.png';
 import consul from '../img/logo-consul.png';
 import theBar from '../img/logo-thebar.png';
+import jussi from '../img/image-jussi.png';
 //  END - imagens
 
 
@@ -17,31 +18,30 @@ import theBar from '../img/logo-thebar.png';
 function Home() {
   return (
 
-  <div className="HomePage">
-          {/* START - HEADER */}
-        <div className="HomePage__HomeHeader">
-          <div className="HomePage__HomeHeader__logoHeader"><img src={logoJussi} alt={"Logo Jussi"}/></div>
+    <html className="HomePage">
+      <header className="HomePage__HomeHeader">
+        <div className="HomePage__HomeHeader__logoHeader"><img src={logoJussi} alt={"Logo Jussi"} /></div>
 
-          <div className="HomePage__HomeHeader__info-header">
-            <span> Nossas soluções</span>
-            <span> Conheça a Jussi</span>
-          </div>
-
-          <div className="HomePage__HomeHeader__search-header">
-            <span className="HomePage__HomeHeader__search-input">Buscar</span>
-          </div>
-
-          <div className="HomePage__HomeHeader__login-cart">
-            <span className="HomePage__HomeHeader__login">Login</span>
-            <span><img src={carrinho} alt={"Carrinho"}/></span>
-          </div>
-
+        <div className="HomePage__HomeHeader__info-header">
+          <span> Nossas soluções</span>
+          <span> Conheça a Jussi</span>
         </div>
-        {/* END - HEADER */}
 
-        {/* START - banner */}
+        <div className="HomePage__HomeHeader__search-header">
+          <span className="HomePage__HomeHeader__search-input">Buscar</span>
+        </div>
+
+        <div className="HomePage__HomeHeader__login-cart">
+          <span className="HomePage__HomeHeader__login">Login</span>
+          <span><img src={carrinho} alt={"Carrinho"} /></span>
+        </div>
+
+      </header>
+
+      <body>
+
         <div className="HomePage__banner">
-            <div className="HomePage__banner__conteudo">
+          <div className="HomePage__banner__conteudo">
             <span className="HomePage__banner__details">//</span>
             <span className="HomePage__banner__text">criamos lojas que vendem mais.</span>
             <span className="HomePage__banner__text-dois">A Jüssi é especialista na criação de lojas usando a plataforma VTEX. Precisa criar sua loja ou migrar de plataforma?</span>
@@ -49,27 +49,23 @@ function Home() {
 
             <div className="HomePage__banner__products">
               <div className="product-left">
-                <img src={geladeira} alt={"product"}/>
+                <img src={geladeira} alt={"product"} />
               </div>
             </div>
 
-            </div>
+          </div>
         </div>
-        {/* END - banner */}
 
-        {/* START - tipar lojas */}
         <div className="tipbar">
 
           <span>Nossas principais lojas VTEX</span>
-          <div><img src={brastemp} alt={"Brastemp"}/></div>
-          <div><img src={compraCerta} alt={"Compra Certa"}/></div>
-          <div><img src={consul} alt={"Consul"}/></div>
-          <div><img src={theBar} alt={"The Bar"}/></div>
+          <div><img src={brastemp} alt={"Brastemp"} /></div>
+          <div><img src={compraCerta} alt={"Compra Certa"} /></div>
+          <div><img src={consul} alt={"Consul"} /></div>
+          <div><img src={theBar} alt={"The Bar"} /></div>
 
         </div>
-        {/* END - tipar lojas */}
 
-        {/* START - products */}
         <div className="soluctions">
           <h1>Nossas Soluções</h1>
           <div className="soluctions__products">
@@ -93,7 +89,7 @@ function Home() {
               <div>Feature 3</div>
               <div>Ver solução</div>
             </div>
-            
+
             <div className="soluctions__product-three">
               <div>P3</div>
               <div>Nome do Produto #3</div>
@@ -118,45 +114,69 @@ function Home() {
 
         </div>
 
-        {/* END - products */}
 
-        {/* START - quem somos */}
 
-        <div className="">
-          <div className="">
-            
+        <div className="quemSomos">
+
+          <div className="quemSomos__textContent">
+            <div className="quemSomos__title"> Olá, somos a Jüssi</div>
+            <div className="quemSomos__text">
+            A Jüssi é uma agência integrante do grupo global WPP que vem há 10 anos consolidando o pensamento voltado para produtos e resolução de problemas. Nosso área dedicada exclusivamente para Produtos Digitais é organizada em 6 especialidades: Product Managamenet, User Experience Design, SEO, Tecnologia, Agile e User Behavior Analytics.
+            </div>
           </div>
-          
-          <div className="">
+
+          <div className="quemSomos__imagem">
+          <img src={jussi} alt={"Jussi"} />
+          </div>
+
+        </div>
+
+
+
+
+
+        <div className="informationsContent">
+
+          <div className="informationsContent__text">
+
+            <div>
+            Essa loja foi construída usando uma das nossas soluções da plataforma VTEX. tenha a sua. 
+            </div>
+            <div>Entre em contato</div>
+            <div>comercial@jussi.com.br</div>
+
+          </div>
+
+        </div>
+
+        <div className="newsletterContent">
+          <div className="newsletterContent__text">
+          receba novidades da nossa área de produtos digitais.
+          </div>
+
+          <div className="newsletterContent__input">
+
+          <div>Digite seu email</div>
+          <div>cadastrar</div>
 
           </div>
         </div>
 
-        {/* END - quem somos */}
+      </body>
 
-        {/* START - footer informations */}
-        <div>
-          <div>
+      <footer className="footerContent">
+        <div className="footerContent__logo-company">
 
-          </div>
         </div>
-        {/* END - footer informations */}
+        <div className="footerContent__redes-sociais">
 
-        {/* START - footer */}
-        <div>
-          <div>
-
-          </div>
-          <div>
-            
-          </div>
         </div>
-        {/* END - footer */}
+      </footer>
 
 
 
 
-    </div>
+    </html>
 
 
   );
